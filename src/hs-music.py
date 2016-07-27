e40b9a48c97502466837539e2929265e5f16f55b
Areeb Beigh <areebbeigh@gmail.com>
2016-07-27 21:50:30 +0530

    Extended functionality to play music files from multiple directories and sub-directories

--- src/hs-music.py
+++ src/hs-music.py
@@ -2,8 +2,8 @@
 # Created: 10th April 2016
 
 ''' 
-Description: Makes a temporary playlist of the music files in the folder in 
-config.ini [hs-music] and opens it with the default media player  
+Makes a temporary playlist of the music files in the directories in config.ini 
+[hs-music] and opens it with the default media player
 '''
 
 import os, configparser, initialize
@@ -12,17 +12,53 @@ Config = initialize.Config
 whiteSpace = initialize.whiteSpace
 
 # Gets the music directory from the config.ini [hs-music] section
-directory = Config.get("hs-music", "directory")
+directories = []
+
+for option in Config.options("hs-music"):
+	if (Config.get("hs-music", option)):
+		directories.append(Config.get("hs-music", option))
 
 def main():
 	execute()
 
+def checkExt(fileName):
+	# Takes a file name (string) as an arguement and checks if the file
+	# has valid extensions for a music file
+	
+	extensions = [".mp3", ".m4a", ".ogg", ".flac"]
+
+	for extension in extensions:
+		if (fileName.endswith(extension) or fileName.endswith(extension.upper())):
+			return True
+
+	return False
+
+def getMusicFiles(directory):
+	# Returns a list of all the music files from a given directory and 
+	# it's sub-directories
+
+	musicFiles = []
+	files = os.listdir(directory)
+	
+	for file in files:
+		# Because the initialize module changes the CWD, 
+		# variable 'file' contain must be the full path to the file
+		file = os.path.join(directory, file)
+		if (os.path.isdir(file)):
+			musicFiles.extend(getMusicFiles(file))
+		elif(checkExt(file)):
+			musicFiles.append(file)
+
+	return musicFiles
+
 def execute():
 	# If the directory has been specified in config.ini then ...
-	if (directory):
-		# Gets a list of the files in the specified directory
-		files = os.listdir(directory)
+	if (directories):
+		files = []
 		
+		for directory in directories:
+			files.extend(getMusicFiles(directory))
+
 		# Variable to store the file count in the directory
 		fileCount = 0
 
@@ -31,20 +67,21 @@ def execute():
 		# Creates a temporary playlist
 		with open('playlist.m3u', 'w+') as playlist:
 			for file in files:
-				# If the file has a .mp3 extension then ...
-				if (file.endswith('.mp3') or file.endswith(".MP3")):
-					# Writes the file path to the temporary playlist.m3u
-					playlist.write(os.path.join(directory, file) + '\n')
-					fileCount += 1
+				# Writes the file path to the temporary playlist.m3u
+				playlist.write(file + '\n')
+				fileCount += 1
 		
 		# Print the number of mp3 files added to playlist.m3u
 		print("{0} {1} mp3 files detected".format(whiteSpace, fileCount))
 		playlist.close()
 
+		m3uFile = os.path.join(os.getcwd(), "playlist.m3u")
+
 		# Opens the file with the default media player
-		os.startfile('playlist.m3u')
+		os.startfile(m3uFile)
 		# delete_playlist.py deletes the temporary playlist
-		os.system("START \"delete_playlist.py\" /MIN python \"bin\\delete_playlist.py\"")
+		os.system(
+			"START \"delete_playlist.py\" /MIN python \"bin\\delete_playlist.py\"")
 
 	# If the directory is not specified in config.ini then ...
 	else:
