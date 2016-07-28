cbcaa4d63936b6d3872a8f8b5e04def615cab407
Areeb Beigh <areebbeigh@gmail.com>
2016-07-28 17:55:42 +0530

    Moved modules to src

--- scripts/src/initialize.py
+++ scripts/src/initialize.py
@@ -7,6 +7,21 @@ import os
 import sys
 import configparser
 
+# Errors
+class Error(Exception):
+	"""Base class for all exceptions."""
+	pass
+
+class ConfigError(Error):
+	"""Raised when invalid configuration is encountered
+	the configuration file
+	"""
+
+	def __init__(self, message):
+		self.message = message
+	def __srt__(self):
+		return repr(self.message)
+
 # Gets the root directory (Drive letter in case of windows)
 rootDirectory = os.path.splitdrive(sys.executable)[0]
 
