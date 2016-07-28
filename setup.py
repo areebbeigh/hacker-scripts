6802b1457db51182275fad4d5f4bd523e271479b
Areeb Beigh <areebbeigh@gmail.com>
2016-07-28 17:56:07 +0530

    Added feature to keep previous configuration

--- setup.py
+++ setup.py
@@ -1,30 +1,49 @@
 # Setup for installing hacker-scripts
-import os, sys, shutil
+import os
+import sys
+import shutil
+import time
+
 from sys import argv
 
 script, arguement = argv
 
 # Gets the root directory (Drive letter for Windows)
 rootDirectory = os.path.splitdrive(sys.executable)[0] + "\\"
-
 mainDirectory = os.path.join(rootDirectory, "hacker-scripts")
-scriptsDirectory = os.path.join(mainDirectory, "Scripts")
+scriptsDirectory = os.path.join(mainDirectory, "scripts")
+
+backupDirName = "hs_backup " + time.strftime("%D %H_%M_%S").replace('/', '-')
+backupDir = os.path.join(rootDirectory, backupDirName)
+configFile = os.path.join(mainDirectory, "config.ini")
+
+oldConfig = ""
 
 def main():
-	# If the directory hacker-scripts already exists in the root directory then ...
+	global oldConfig
+
 	if (os.path.exists(mainDirectory)):
-		# Asks the user whether to overrite the directory 
 		print("A directory named \"hacker-scripts\" already exists in " +
-		"{}, do you want to replace it? (y/N)".format(rootDirectory))
+		"{}, do you want to replace it? (Y/N)".format(rootDirectory))
 
-		# Changes user provided input to upper case
 		choice = input().upper()
+		affirmitive = ["Y", "YES"]
 
-		if(choice in "Y YES".split()):
-			# Removes the entire directory tree
-			shutil.rmtree(mainDirectory)
-			# Proceed with the installation
-			install()
+		if(choice in affirmitive):
+			# Creates a backup of existing hacker-scripts directory
+			# for roll back in case it is needed
+			shutil.copytree(mainDirectory, backupDir)
+
+			if(os.path.exists(configFile)):
+				print("Do you want to keep your previous config file? (Y/N)")
+				choice = input().upper()
+				if(choice in affirmitive):
+					with open(configFile, "r") as f:
+						oldConfig = f.read()
+			try:
+				install(True)
+			except Exception as e:
+				rollBack(e)
 		else:
 			sys.exit()
 	
@@ -32,46 +51,59 @@ def main():
 	else:
 		install()
 
-def install():
-	# Creates the directories hacker-scripts and hacker-scripts\bin in the root directory
-	os.mkdir(mainDirectory)
-	os.mkdir(scriptsDirectory)
+def rollBack(error):
+	"""Restores previous installation of hacker-scripts if an error occurs
+	while overwriting it"""
 
-	# Scripts to be installed are in the "scripts" directory of the downloaded package os.listdir() returns the list
-	scripts = os.listdir("scripts")
+	try:
+		shutil.rmtree(mainDirectory)
+	except FileNotFoundError:
+		pass
+	finally:
+		print("-------")
+		print("An unexpected error occured, setup will attempt to revert changes.")
+		print("You can report the issue at github.com/areeb-beigh if it is a bug.")
+		print("-------")
 
-	for script in scripts:
-		script = os.path.join("scripts", script)
-		exclusions = [
-			os.path.join("scripts", "hs-config.py"),
-			]
+		shutil.copytree(backupDir, mainDirectory)
+		shutil.rmtree(backupDir)
+		print("Changes reverted successfuly")
+		print("Error that occured:")
+		raise error
 
-		if (not os.path.isdir(script)):
-			if(script not in exclusions):
-				shutil.copy(script, scriptsDirectory)
-			elif(script == exclusions[0]):
-				shutil.copy(script, mainDirectory)
+def install(overwrite):
+	if(overwrite):
+		shutil.rmtree(mainDirectory)
 
-	# Adds C:\hacker-scripts and <root directory>\hacker-scripts\Scripts to PATH
+	shutil.copytree("scripts", scriptsDirectory)
+
+	# Restores the old configuration file
+	if(oldConfig):
+		with open(configFile, "w+") as f:
+			f.write(oldConfig)
+
+	# Adds <root directory>\hacker-scripts\scripts to PATH
 	path = os.environ['PATH']
 
-	if(mainDirectory not in path.split(";") or scriptsDirectory not in path.split(";")):
+	if(scriptsDirectory not in path.split(";")):
 		os.system("setx PATH \"\"")
-		os.system("setx PATH \"{};{};{}\"".format(
-			path, 
-			mainDirectory, 
+		os.system("setx PATH \"{};{}\"".format(
+			path,
 			scriptsDirectory))
 
 	print("""
 	The installation was successful, you can use the command \"hs-help\" in a 
 	new terminal to view the help documentation.
 	
+	(If you saved the previous configuration you can skip this):
 	Please open an new terminal session and run hs-config to configure hacker-scripts.
-	If you face any problems after installation please check the documentation at 
-	github.com/areeb-beigh/hacker-scripts 
+	If you face any problems after installation please check the documentation in
+	the readme.md file or at github.com/areeb-beigh/hacker-scripts
 	""")
+
+	shutil.rmtree(backupDir)
 
 if arguement == "install":
 	main()
 else:
-	raise ValueError("Invalid arguement, pass arguement \"install\" to install hacker-scripts")
+	print("usage: setup.py install")
