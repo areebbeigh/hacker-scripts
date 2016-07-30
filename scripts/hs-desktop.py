eda6ebae4959e78af1388ee62fca262529bc5dd1
Areeb Beigh <areebbeigh@gmail.com>
2016-07-30 13:38:17 +0530

    Wrong script - Don't code while you're sleepy

--- scripts/hs-desktop.py
+++ scripts/hs-desktop.py
@@ -1,96 +1,124 @@
 # Author: Areeb Beigh
-# Created: 28th July 2016
+# Created: 10th April 2016
 
 '''
-Automatically creates a backup of all the files on the drive. Both, 
-the files and the backup drive are specified in the configuration
+Manages your desktop by automatically placing different files to their 
+respective directories in config.ini [hs-desktop]
 '''
 
 import os
+import random
+
 from src.initialize import *
 
-'''
-purge (bool)
-retries (int)
-backup_drive
-file1
-file2
-.
-.
-.
-'''
+# Gets the path to Desktop
+desktop = os.path.join(os.environ['USERPROFILE'], "Desktop")
+
+# Paths/Directories for different file types are loaded from the 
+# config.ini [hs-desktop] seciton
+paths = { 
+	"images": Config.get("hs-desktop", "images_directory"),
+	"videos": Config.get("hs-desktop", "videos_directory"),
+	"textFiles": Config.get("hs-desktop", "files_directory"),
+	}
+
+# Different extension types for different file types
+imageExtensions = [
+		".png", 
+		".jpg", 
+		".gif",
+		]
+videoExtensions = [
+		".avi", 
+		".mp4", 
+		".mkv", 
+		".flv", 
+		".wmv", 
+		".m4v", 
+		".mpg", 
+		".3gp", 
+		".3g2", 
+		".f4v"
+		]
+textFileExtensions = [
+		".txt", 
+		".ppt", 
+		".doc", 
+		".xls",
+		]
 
 def main():
 	execute()
 
-def isValidConfig(purge, retries, backupLocation, files):
-	"""Takes all options in the "hs-backup" section as parameters and returns
-	True if they are valid, else raises a ConfigError"""
-
-	try:
-		purge = int(purge)
-		if(not(purge == 0 or purge == 1)):
-			raise ValueError
-	except ValueError:
-		raise ConfigError("Value of purge must be either 0 or 1")
-
-	try:
-		retries = int(retries)
-		if(not(retries > 0)):
-			raise ValueError
-	except ValueError:
-		raise ConfigError("Value of retries must be an integer greater than 0")
-
-	if(not os.path.isdir(backupLocation) and backupLocation != ""):
-		return ConfigError("Value of backup_location must be a valid directory (available)")
-
-	for file in files:
-		if(not os.path.isdir(file)):
-			raise ConfigError("{} is not a directory".format(file))
-
-	return True
-
 def execute():
-	purge = Config.get("hs-backup", "purge")
-	retries = Config.get("hs-backup", "retries")
-	backupLocation = Config.get("hs-backup", "backup_location")
-	directories = []
-
-	logFile = "backup_log.txt"
-
-	for option in Config.options("hs-backup"):
-		if(option[0:9] == "directory"):
-			directories.append(Config.get("hs-backup", option))
-
-	if(isValidConfig(purge, retries, backupLocation, directories)):
-		purge = int(purge)
-		retries = int(retries)
-
-		for file in directories:
-			backup = os.path.join(backupLocation, os.path.basename(file))
+	# If the path values are not set then this block wont execute
+	if ("" not in paths.values()):
+		files = os.listdir(desktop)
 		
-			if(not os.path.exists(backup)):
-				os.makedirs(backup)
+		for file in files:
+			# Creates a full path to the file in desktop
+			filePath = os.path.join(desktop, file)
 
-			src = file
-			dst = backup
+			if not(os.path.isdir(file)):
+				# Splits file path and file extension
+				fileName, fileExtension = os.path.splitext(filePath)
 
-			print(file, backup, sep=" -> ")
+				# Further we check if the extension of the file is mentioned in 
+				# any of the extensions lists above and then we put the file in 
+				# the respective directory
 
-			if(purge == 0):
-				command = "ROBOCOPY /LOG+:\"{0}\" /V /E /R:{1} {2} {3}".format(
-					logFile,
-					retries,
-					src,
-					dst)
-			elif(purge == 1):
-				command = "ROBOCOPY /LOG+:\"{0}\" /V /E /PURGE /R:{1} {2} {3}".format(
-					logFile,
-					retries,
-					src,
-					dst)
+				# Check for images
+				if fileExtension.lower() in imageExtensions:
+					newLocation = os.path.join(paths["images"], file)
 
-			os.system(command)
-		
-if(__name__ == "__main__"):
+				# Check for videos
+				elif fileExtension.lower() in videoExtensions:
+					newLocation = os.path.join(paths["videos"], file)
+
+				# Check for text files
+				elif fileExtension.lower() in textFileExtensions:
+					newLocation = os.path.join(paths["textFiles"], file)
+
+				# If the file type is unknown to the program then we skip it
+				else:
+					newLocation = None
+				
+				# If the file type is not unknown then ...
+				if (newLocation):
+					try:
+						# This moves the file to "newLocation"
+						os.rename(filePath, newLocation)
+						print("{0} Moved {1}".format(
+							whiteSpace, 
+							os.path.basename(filePath)))
+
+					# If a file with the same name already exists then we rename 
+					# the file we're operating on
+					except (WindowsError):
+						fileName, fileExtension = os.path.splitext(newLocation)
+						count = 1
+						# Randomly append an integer to the end of the file name
+						while (os.path.exists(newLocation)):
+							newLocation = (
+								fileName         + 
+								" ("             + 
+								str(count)       + 
+								")"              +
+								fileExtension
+								)
+
+							count += 1
+
+						# Moves the file to "newLocation"
+						os.rename(filePath, newLocation)
+						print("{0} Renamed {1} to {2} since {1} already exists in destination folder".format(
+							whiteSpace,
+							os.path.basename(file),
+							os.path.basename(newLocation)))
+
+	# If the directory is not specified in config.ini then ...
+	else:
+		raise ConfigError("All paths are not specified in the configuration file")
+
+if __name__ == "__main__":
 	main()
