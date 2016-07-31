71f398e4a708351a6e7bb756ba3c6c168a118e0b
Areeb Beigh <areebbeigh@gmail.com>
2016-07-31 17:15:51 +0530

    Added add/del arguments and other features
    Tasks can now be scheduled to be executed on any day in the future.
    Already scheduled tasks can also be deleted using the 'del' argument.

--- scripts/hs-schedule.py
+++ scripts/hs-schedule.py
@@ -9,47 +9,107 @@ Command used: SCHTASKS
 '''
 
 import os
+import sys
 import time
+import argparse
+import calendar
+from src import help
+from src import initialize
 
-from src.initialize import *
+initialize.initialize()
+
+whiteSpace = initialize.whiteSpace
 
 def main():
-	execute()
+	cmd = sys.argv[0].partition(".")[0]
+
+	parser = argparse.ArgumentParser(add_help=False)
+	parser.add_argument('action', nargs='?')
+	parser.add_argument('--help', 
+						'-help', 
+						action='store_true')
+
+	args = parser.parse_args()
+
+	if(args.help):
+		help.displayHelp(cmd)
+		return
+
+	if(args.action == "add"):
+		addTask()
+	elif(args.action == "del"):
+		delTask()
+	else:
+		help.displayHelp(cmd)
 
 def getCurrentTime():
-	"""Returns the current time in 24 hr format as a list
-	Hours at index 0 and minutes at index 1"""
+	"""Returns the current time in 24 hr format"""
 
-	return time.strftime("%H:%M").split(":")
+	return time.strftime("%H:%M")
 
-def isValidTime(givenTime):
-	"""Given a list time this function checks if the given time is 
-	in the future"""
-	
-	currentTime = getCurrentTime()
+def getCurrentDate():
+	"""Returns the current date in MM/DD/YYYY format"""
+
+	return time.strftime("%m/%d/%Y")
+
+def isValidTime(givenTime, today):
+	"""Given a time string this function checks if the given time is 
+	in the future today (if "today" is true) else just validates the time"""
+
+	givenTime = givenTime.split(":")
+	currentTime = getCurrentTime().split(":")
 
 	givenHour = int(givenTime[0])
 	givenMinute = int(givenTime[1])
 
 	currentHour = int(currentTime[0])
 	currentMinute = int(currentTime[1])
-
-	if (givenHour < 24 and givenMinute <= 59):
-		return (
-			(givenHour > currentHour) or
-			(givenHour == currentHour and givenMinute > currentMinute)
-			)
+	
+	if(today):
+		if(givenHour < 24 and givenMinute <= 59):
+			return (
+				(givenHour > currentHour) or
+				(givenHour == currentHour and givenMinute > currentMinute)
+				)
+	else:
+		return (givenHour < 24 and givenMinute <= 59)
 	
 	return False
 
-def execute():
+def isValidDate(givenDate):
+	"""Given a date string this function checks if the date is in the future"""
+
+	givenDate = givenDate.split("/")
+	currentDate = getCurrentDate().split("/")
+
+	givenDay = int(givenDate[1])
+	givenMonth = int(givenDate[0])
+	givenYear = int(givenDate[2])
+
+	currentDay = int(currentDate[1])
+	currentMonth = int(currentDate[0])
+	currentYear = int(currentDate[2])
+
+	try:
+		calendar.weekday(givenYear, givenMonth, givenDay)
+	except ValueError:
+		return False
+
+	return (
+		(givenYear == currentYear and givenMonth == currentMonth and givenDay > currentDay) or
+		(givenYear == currentYear and givenMonth > currentMonth) or
+		(givenYear > currentYear))
+
+	return False
+
+def addTask():
 	"""Takes all the information about the task to be scheduled and
 	creates the scheduled task"""
 
 	commands = []
 
-	# List of ALL the files present in the bin directory
-	scripts = os.listdir(os.path.join(os.getcwd(), "Scripts"))
+	# List of ALL the files present in the scripts directory
+	scripts = os.listdir(os.path.join(os.getcwd(), "scripts"))
 
 	for script in scripts:
 		# Appends all the script names that have "hs-" prefix and ".py" extension
@@ -59,30 +119,71 @@ def execute():
 			cmdName = script.partition(".")[0]
 			commands.append(cmdName)
 	
-	givenCmd = input("\n{}Command Name: ".format(whiteSpace)).rstrip()
-	execTime = ""
+	while True:
+		taskName = input("{} Task name: ".format(whiteSpace))
 
-	if(givenCmd in commands):
-		while True:
-			execTime_ = input("{}Execution Time (ex: 18:51): ".format(whiteSpace))
-			
-			if(isValidTime(execTime_.split(":"))):
-				execTime += execTime_
-				break
-			else:
-				print("{}Invalid time, the time must be in the future today".format(whiteSpace))
+		if(taskName):
+			break
+		else:
+			print("{} Invalid task name".format(whiteSpace))
 
-	else:
-		raise ValueError("{} does not exist in hacker-scripts commands".format(givenCmd))
+	while True:
+		cmdName = input("{} Command name: ".format(whiteSpace))
 
-	execCmd = os.path.join(os.getcwd(), "Scripts", givenCmd + ".py")
-	execName = "[{0}]_hacker-scripts_{1}".format(execTime.replace(":", "_"), givenCmd)
+		if(cmdName in commands):
+			break
+		else:
+			print("{} Invalid command".format(whiteSpace))
 
-	print()
+	while True:
+		date = input("{} Date (leave blank if today) [MM/DD/YYYY]: ".format(whiteSpace))
 
-	# Creates a new scheduled task
-	os.system("SCHTASKS /Create /SC ONCE /ST {0} /TR \"python '{1}'\" /TN {2}".format(
-		execTime, execCmd, execName))
+		if(date == "" or isValidDate(date)):
+			break
+		else:
+			print("{} Invalid date".format(whiteSpace))
+
+	if(date == ""):
+		date = getCurrentDate()
+
+	while True:
+		taskTime = input("{} Time [HH:MM]: ".format(whiteSpace))
+
+		if(date == getCurrentDate()):
+			check = isValidTime(taskTime, True)
+		else:
+			check = isValidTime(taskTime, False)
+
+		if(check):
+			break
+		else:
+			print("{} Invliad time".format(whiteSpace))
+
+	cmdName = os.path.join(os.getcwd(), "scripts", cmdName + ".py")
+	
+	command = "SCHTASKS /CREATE /SC ONCE /TN {0} /SD {1} /ST {2} /TR \"python '{3}'\"".format(
+				taskName,
+				date,
+				taskTime,
+				cmdName)
+
+	os.system(command)
+	
+def delTask():
+	"""Asks the user for the task name to delete and attempts to delete
+	that task"""
+
+	while True:
+		taskName = input("{} Enter task name: ".format(whiteSpace))
+
+		if(taskName):
+			break
+		else:
+			print("{} Invalid task name".format(whiteSpace))
+
+	command = "SCHTASKS /DELETE /TN {}".format(taskName)
+
+	os.system(command)
 
 if(__name__ == "__main__"):
 	main()
