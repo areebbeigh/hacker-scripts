bc9580dd4035d73baa11cda4848eeabd9cd3684c
Areeb Beigh <areebbeigh@gmail.com>
2016-08-03 17:24:21 +0530

    Add directories absolute path to PATH instead of CWD
    os.getcwd() was not a clean way to get the hacker-scripts' directory so
    now the script uses
    
    `os.path.dirname(os.path.abspath(__file__)) `
    
    to get the hacker-scripts' directory

--- add_to_path.py
+++ add_to_path.py
@@ -4,15 +4,15 @@ Adds the current working directory to environment variable PATH
 
 import os
 
-currentDir = os.getcwd()
+absDirPath = os.path.dirname(os.path.abspath(__file__))
 PATH = os.environ['PATH']
 
-if currentDir not in PATH.split(";"):
+if absDirPath not in PATH.split(";"):
     os.system("setx PATH \"\"")
-    os.system("setx PATH \"{0};{1}\"".format(PATH, currentDir))
+    os.system("setx PATH \"{0};{1}\"".format(PATH, absDirPath))
 
 print("""
-        This directory has been added to PATH.
+        The hacker-scripts has been added to PATH.
         -----
         To view the hacker-scripts commands from your terminal,
         open a new terminal window and type hs-help.
