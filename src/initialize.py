1f47a302a6e6e126c0ef4cf0947e2966dc305235
Areeb Beigh <areebbeigh@gmail.com>
2016-08-05 20:35:40 +0530

    config_file, BASE_DIRECTORY, white_space are properties

--- src/initialize.py
+++ src/initialize.py
@@ -2,21 +2,34 @@
 This script is used in most of the hacker-script programs to do the initial stuff
 """
 
-import configparser
 import os
 
 
 class Initialize:
-    """ This class does the initial stuff such as changing the CWD
-    and creating a ConfigParser object to read the configuration
-    file """
+    """
+    Holds values for the base directory, white space and
+    the configuration file name as attributes to be accessed by other
+    scripts
+    """
 
     def __init__(self):
-        # Changes the current working directory to the hacker-scripts base directory
-        self.BASE_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        os.chdir(self.BASE_DIRECTORY)
+        # Sets the base directory
+        self._BASE_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         # White space appended before printed statements
-        self.whiteSpace = "    "
-        # Creates a ConfigParser object and reads the configuration file
-        self.configFile = "config.ini"
+        self._white_space = "    "
+        # The configuration file name
+        self._config_file = "config.ini"
+        # Changes the current working directory to the base directory
+        os.chdir(self._BASE_DIRECTORY)
 
+    @property
+    def BASE_DIRECTORY(self):
+        return self._BASE_DIRECTORY
+
+    @property
+    def white_space(self):
+        return self._white_space
+
+    @property
+    def config_file(self):
+        return self._config_file
