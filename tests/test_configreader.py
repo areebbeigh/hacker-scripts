537f640a34fbebe3991d69891c2dde025a56c9c3
Areeb Beigh <areebbeigh@gmail.com>
2016-08-13 21:56:53 +0530

    Optimized tests

--- tests/test_configreader.py
+++ tests/test_configreader.py
@@ -15,39 +15,37 @@ class ConfigReaderTestCase(unittest.TestCase):
     """
 
     def setUp(self):
-        self.Config = configparser.ConfigParser()
-        self.Config.read("tests/test_config.ini")
-        self.reader = configreader.ConfigReader("")
+        self.reader = configreader.ConfigReader("tests/test_config.ini")
 
     def test_read_backup(self):
-        purge, retries, backup_loc, directories = self.reader._read_backup(self.Config)
+        purge, retries, backup_loc, directories = self.reader.read_config("hs-backup")
         self.assertEqual(purge, '1')
         self.assertEqual(retries, '10')
         self.assertEqual(backup_loc, "D:/")
         self.assertEqual(directories, ["C:/Sample_Dir1", "C:/Sample_Dir2"])
 
     def test_read_browse(self):
-        urls = self.reader._read_browse(self.Config)
+        urls = self.reader.read_config("hs-browse")
         self.assertEqual(urls, ["www.sampleweb1.com", "www.sampleweb2.com"])
 
     def test_read_manage(self):
         with self.assertRaises(ConfigError):
-            self.reader._read_manage(self.Config)
+            self.reader.read_config("hs-manage")
 
     def test_read_music(self):
-        result = self.reader._read_music(self.Config)
+        result = self.reader.read_config("hs-music")
         self.assertEqual(result, [])
 
     def test_read_start(self):
-        result = self.reader._read_start(self.Config)
+        result = self.reader.read_config("hs-start")
         self.assertEqual(result, ["C:/Sample_Dir1/Sample_Prog1.exe"])
 
     def test_read_wallpaper(self):
-        result = self.reader._read_wallpaper(self.Config)
+        result = self.reader.read_config("hs-wallpaper")
         self.assertEqual(result, [])
 
-    def test_read_word(self):
-        files, editor = self.reader._read_work(self.Config)
+    def test_read_work(self):
+        files, editor = self.reader.read_config("hs-work")
         self.assertEqual(files, ["C:/Sample_Dir1"])
         self.assertEqual(editor, "C:/Sample_Editor1.exe")
 
