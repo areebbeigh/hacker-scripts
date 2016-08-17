63d8bc650c182cc571e6e24489b42371d491e1af
Areeb Beigh <areebbeigh@gmail.com>
2016-08-17 17:00:51 +0530

    Improved tests with mock config and directories

---
+++ tests/configgenertor.py
@@ -0,0 +1,50 @@
+# Python imports
+import os
+
+# Local imports
+from src.initialize import Initialize
+
+
+def generate_config_string():
+    """ Generates a configuration string with mock values and  returns it """
+
+    initializer = Initialize()
+    test_directory = os.path.join(initializer.BASE_DIRECTORY, "tests","test_directory")
+    test_locations = {
+        'ext_dir_1': os.path.join(test_directory, "hs_manage", "extn_dir_1"),
+        'ext_dir_2': os.path.join(test_directory, "hs_manage", "extn_dir_2"),
+        'music_dir_1': os.path.join(test_directory, "hs_music"),
+        'wallpaper_dir_1': os.path.join(test_directory, "hs_wallpaper"),
+    }
+
+    test_config = """[hs-backup]
+    purge            = 1
+    retries          = 10
+    backup_location  = D:/
+    directory1       = C:/Sample_Dir1
+    directory2       = C:/Sample_Dir2
+
+    [hs-browse]
+    url1 = www.sampleweb1.com
+    url2 = www.sampleweb2.com
+
+    [hs-manage]
+    extension_set_1  = .txt, .pptx
+    location_1       = %(ext_dir_1)s
+    extension_set_2  = .jpg, .png
+    location_2       = %(ext_dir_2)s
+
+    [hs-music]
+    directory1 = %(music_dir_1)s
+
+    [hs-start]
+    program1 = C:/Sample_Dir1/Sample_Prog1.exe
+
+    [hs-wallpaper]
+    directory1 = %(wallpaper_dir_1)s
+
+    [hs-work]
+    editor   = C:/Sample_Editor1.exe
+    project1 = C:/Sample_Dir1
+    """ % test_locations
+    return test_config
--- tests/test_configreader.py
+++ tests/test_configreader.py
@@ -1,51 +1,74 @@
 # Python imports
-import unittest
 import configparser
+import unittest
+import os
 
 # Local imports
 from src import configreader
 from src.errors import ConfigError
+from src.initialize import Initialize
+from tests.configgenertor import generate_config_string
+
+initializer = Initialize()
+test_directory = os.path.join(initializer.BASE_DIRECTORY, "tests","test_directory")
+test_config = generate_config_string()
 
 
 class ConfigReaderTestCase(unittest.TestCase):
     """
-    These tests use the "test_config.ini" file as input to the
-    configreader.ConfigReader() class methods and tests whether the return
-    values are the same as in the mock configuration file
+    These tests are written for the individual read methods in the
+    configreader.ConfigReader() module. A mock configuration and
+    directory tree (tests/test_directory) is used for testing.
     """
 
     def setUp(self):
-        self.reader = configreader.ConfigReader("tests/test_config.ini")
+        self.reader = configreader.ConfigReader("")
+        self.Config = configparser.ConfigParser()
+        self.Config.read_string(test_config)
 
     def test_read_backup(self):
-        purge, retries, backup_loc, directories = self.reader.read_config("hs-backup")
+        purge, retries, backup_loc, directories = self.reader._read_backup(self.Config)
         self.assertEqual(purge, '1')
         self.assertEqual(retries, '10')
         self.assertEqual(backup_loc, "D:/")
         self.assertEqual(directories, ["C:/Sample_Dir1", "C:/Sample_Dir2"])
 
     def test_read_browse(self):
-        urls = self.reader.read_config("hs-browse")
+        urls = self.reader._read_browse(self.Config)
         self.assertEqual(urls, ["www.sampleweb1.com", "www.sampleweb2.com"])
 
     def test_read_manage(self):
-        with self.assertRaises(ConfigError):
-            self.reader.read_config("hs-manage")
+        result = self.reader._read_manage(self.Config)
+        expected_result = {
+            ('.txt', '.pptx'): os.path.join(test_directory, "hs_manage", "extn_dir_1"),
+            ('.jpg', '.png'): os.path.join(test_directory, "hs_manage", "extn_dir_2"),
+        }
+        self.assertEqual(result, expected_result)
 
     def test_read_music(self):
-        result = self.reader.read_config("hs-music")
-        self.assertEqual(result, [])
+        result = self.reader._read_music(self.Config)
+        expected_result = [
+            os.path.join(test_directory, "hs_music", "fake_music1.mp3"),
+            os.path.join(test_directory, "hs_music", "fake_music2.mp3"),
+            os.path.join(test_directory, "hs_music", "more music","fake_music3.mp3"),
+        ]
+        self.assertEqual(result, expected_result)
 
     def test_read_start(self):
-        result = self.reader.read_config("hs-start")
+        result = self.reader._read_start(self.Config)
         self.assertEqual(result, ["C:/Sample_Dir1/Sample_Prog1.exe"])
 
     def test_read_wallpaper(self):
-        result = self.reader.read_config("hs-wallpaper")
-        self.assertEqual(result, [])
+        result = self.reader._read_wallpaper(self.Config)
+        expected_result = [
+            os.path.join(test_directory, "hs_wallpaper", "fake_wallpaper1.jpg"),
+            os.path.join(test_directory, "hs_wallpaper", "fake_wallpaper2.jpg"),
+            os.path.join(test_directory, "hs_wallpaper", "more wallpapers","fake_wallpaper3.jpg"),
+        ]
+        self.assertEqual(result, expected_result)
 
     def test_read_work(self):
-        files, editor = self.reader.read_config("hs-work")
+        files, editor = self.reader._read_work(self.Config)
         self.assertEqual(files, ["C:/Sample_Dir1"])
         self.assertEqual(editor, "C:/Sample_Editor1.exe")
 
